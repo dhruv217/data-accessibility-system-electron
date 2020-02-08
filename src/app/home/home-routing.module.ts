882f5dab32d0dfb95b2fba3fd9eca5a0e7b9d68c
Dhruv Tripathi <dhruv@pop-os.localdomain>
2020-02-08 13:54:27 +0530

    Remove Charts.

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -4,7 +4,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { AllGaugesComponent } from './components/gauge/all-gauges/all-gauges.component';
-import { AllChartsComponent } from './components/all-charts/all-charts.component';
 import { ExportableTableComponent } from './components/exportable-table/exportable-table.component';
 
 const routes: Routes = [
@@ -14,7 +13,6 @@ const routes: Routes = [
     children: [
       { path: '', component: WelcomeComponent },
       { path: 'gauges', component: AllGaugesComponent },
-      { path: 'charts', component: AllChartsComponent },
       { path: 'table', component: ExportableTableComponent },
     ]
   }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,14 +5,13 @@ import {
   MatDialogModule,
   MatSidenavModule,
   MatCardModule,
-  MatIconModule ,
+  MatIconModule,
   MatToolbarModule,
   MatListModule,
   MatButtonModule,
   MatTableModule
 } from "@angular/material";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { ChartsModule } from 'ng2-charts';
 import { GaugesModule } from "ng-canvas-gauges";
 
 import { HomeRoutingModule } from "./home-routing.module";
@@ -20,24 +19,32 @@ import { HomeRoutingModule } from "./home-routing.module";
 import { HomeComponent } from "./home.component";
 import { SharedModule } from "../shared/shared.module";
 
-import { TempratureGaugeComponent } from './components/gauge/temprature-gauge/temprature-gauge.component';
-import { LargeTempratureGaugeComponent } from './components/gauge/large-temprature-gauge/large-temprature-gauge.component';
-import { AirflowGaugeComponent } from './components/gauge/airflow-gauge/airflow-gauge.component';
-import { RpmGaugeComponent } from './components/gauge/rpm-gauge/rpm-gauge.component';
-import { FuelflowGaugeComponent } from './components/gauge/fuelflow-gauge/fuelflow-gauge.component';
-import { WelcomeComponent } from './components/welcome/welcome.component';
-import { AllGaugesComponent } from './components/gauge/all-gauges/all-gauges.component';
-import { AllChartsComponent } from './components/all-charts/all-charts.component';
-import { ExportableTableComponent } from './components/exportable-table/exportable-table.component';
+import { TempratureGaugeComponent } from "./components/gauge/temprature-gauge/temprature-gauge.component";
+import { LargeTempratureGaugeComponent } from "./components/gauge/large-temprature-gauge/large-temprature-gauge.component";
+import { AirflowGaugeComponent } from "./components/gauge/airflow-gauge/airflow-gauge.component";
+import { RpmGaugeComponent } from "./components/gauge/rpm-gauge/rpm-gauge.component";
+import { FuelflowGaugeComponent } from "./components/gauge/fuelflow-gauge/fuelflow-gauge.component";
+import { WelcomeComponent } from "./components/welcome/welcome.component";
+import { AllGaugesComponent } from "./components/gauge/all-gauges/all-gauges.component";
+import { ExportableTableComponent } from "./components/exportable-table/exportable-table.component";
 
 @NgModule({
-  declarations: [HomeComponent, TempratureGaugeComponent, LargeTempratureGaugeComponent, AirflowGaugeComponent, RpmGaugeComponent, FuelflowGaugeComponent, WelcomeComponent, AllGaugesComponent, AllChartsComponent, ExportableTableComponent],
+  declarations: [
+    HomeComponent,
+    TempratureGaugeComponent,
+    LargeTempratureGaugeComponent,
+    AirflowGaugeComponent,
+    RpmGaugeComponent,
+    FuelflowGaugeComponent,
+    WelcomeComponent,
+    AllGaugesComponent,
+    ExportableTableComponent
+  ],
   imports: [
     CommonModule,
     SharedModule,
     HomeRoutingModule,
     GaugesModule,
-    ChartsModule,
     MatSelectModule,
     MatDialogModule,
     MatSidenavModule,
